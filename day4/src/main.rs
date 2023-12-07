878b0424e1e51d81ea8bcb8ce8f9faccd5c5f867
manorajesh <mano@raje.sh>
2023-12-06 20:53:17 -0800

    Iterating on part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,18 +1,47 @@
+#[derive(Debug, Clone, PartialEq, PartialOrd, Eq, Ord)]
+struct PlayerCard {
+    card: Vec<usize>,
+    winning_card: Vec<usize>,
+    id: usize,
+}
+
+impl PlayerCard {
+    fn new(card: Vec<usize>, winning_card: Vec<usize>, id: usize) -> Self {
+        Self {
+            card,
+            winning_card,
+            id,
+        }
+    }
+
+    fn count(&self) -> usize {
+        let mut count = 0;
+
+        for number in &self.card {
+            if self.winning_card.contains(number) {
+                count += 1;
+            }
+        }
+
+        count
+    }
+}
+
 fn main() {
     let content = std::fs::read_to_string("input.txt").unwrap();
-    let (cards, winning_cards) = extract_data(&content);
+    let player_cards = extract_data(&content);
 
-    let mut total = 0;
-    for (card, winning_card) in cards.iter().zip(winning_cards.iter()) {
-        total += find_matching(card, winning_card);
-    }
+    let total = count_total_cards(&player_cards.clone(), player_cards);
+
+    // println!("Card 1 matches: {}", player_cards[0].count());
 
     println!("Total: {}", total);
 }
 
-fn extract_data(content: &str) -> (Vec<Vec<&str>>, Vec<Vec<&str>>) {
-    let mut winning_numbers: Vec<Vec<&str>> = Vec::new();
-    let mut cards: Vec<Vec<&str>> = Vec::new();
+fn extract_data(content: &str) -> Vec<PlayerCard> {
+    let mut winning_numbers: Vec<Vec<usize>> = Vec::new();
+    let mut cards: Vec<Vec<usize>> = Vec::new();
+    let mut player_cards: Vec<PlayerCard> = Vec::new();
 
     for line in content.lines() {
         let split = line.split(":").nth(1).unwrap();
@@ -29,31 +58,38 @@ fn extract_data(content: &str) -> (Vec<Vec<&str>>, Vec<Vec<&str>>) {
 
                 _ => {
                     if is_card {
-                        current_card.push(number);
+                        current_card.push(number.parse().unwrap());
                     } else {
-                        current_winning_card.push(number);
+                        current_winning_card.push(number.parse().unwrap());
                     }
                 }
             }
         }
         winning_numbers.push(current_winning_card.clone());
+        player_cards.push(
+            PlayerCard::new(
+                current_card.clone(),
+                current_winning_card.clone(),
+                player_cards.len() + 1
+            )
+        );
     }
 
-    (cards, winning_numbers)
+    player_cards
 }
 
-fn find_matching(card: &Vec<&str>, winning_card: &Vec<&str>) -> usize {
-    let mut count = 0;
+fn count_total_cards(all_cards: &Vec<PlayerCard>, copies: Vec<PlayerCard>) -> usize {
+    let mut total_cards = 0;
 
-    for number in card {
-        if winning_card.contains(number) {
-            if count == 0 {
-                count += 1;
-            } else {
-                count *= 2;
-            }
-        }
+    for card in &copies {
+        let next_cards = all_cards
+            .get(card.id..card.count() + card.id)
+            .unwrap_or(&[])
+            .to_vec();
+
+        total_cards += card.count();
+        total_cards += count_total_cards(all_cards, next_cards);
     }
 
-    count
+    total_cards
 }
